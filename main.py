6f13480001a46df1f2aec8e1212a504fc6b6a93b
DN <ryabkova.darya@gmail.com>
2024-12-23 13:32:16 +0300

    .

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import requests
+import json
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
-print(response.text)
+json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
+obj = json.loads(json_text)
+
+key1 = "messages"
+key2 = "message"
+print (obj[key1][1][key2])
