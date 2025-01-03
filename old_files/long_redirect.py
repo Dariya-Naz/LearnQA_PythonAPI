e6c8df2c000cb97faf1deaa04064214f7f9a1d6d
Dariya-Naz <99275108+Dariya-Naz@users.noreply.github.com>
2025-01-03 20:13:24 +0300

    Delete old_files directory

--- old_files/long_redirect.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-response = requests.get("https://playground.learnqa.ru/api/long_redirect")
-list = (response.history)
-amount = len (list)
-print ("Количество редиректов:", amount)
-print ("Конечный url:", response.url)
