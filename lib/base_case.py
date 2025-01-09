ac06977d7212dc751dfbf02f600303d6cc1428ce
DN <ryabkova.darya@gmail.com>
2025-01-09 23:01:07 +0300

    Ex15

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,4 +1,5 @@
 from requests import Response
+from datetime import datetime
 
 class BaseCase:
     def get_cookie (self, response: Response, cookie_name):
@@ -17,3 +18,19 @@ class BaseCase:
 
         assert name in response_as_dict, f"Response JSON doesn't have key {name}"
         return response_as_dict[name]
+
+    def prepare_registration_data(self, email=None, username=None):
+        if email is None:
+            base_part = "learnqa"
+            domain = "example.com"
+            random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+            email = f"{base_part}{random_part}@{domain}"
+        if username is None:
+            username='learnqa'
+        return {
+            'password': '123',
+            'username': username,
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+        }
