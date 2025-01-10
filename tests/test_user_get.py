9138552c411fa0a4896985cb5c75d5244bf7b6b5
DN <ryabkova.darya@gmail.com>
2025-01-10 12:40:49 +0300

    Ex16

---
+++ tests/test_user_get.py
@@ -0,0 +1,64 @@
+from lib.my_requests import MyRequests
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+
+class TestUserGet(BaseCase):
+    def test_get_user_details_not_auth(self):
+        response = MyRequests.get("/user/2")
+        Assertions.assert_json_has_key(response, "username")
+        Assertions.assert_json_has_not_key(response, "email")
+        Assertions.assert_json_has_not_key(response, "firstName")
+        Assertions.assert_json_has_not_key(response, "lastName")
+
+    def test_get_user_details_auth_as_same_user(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response1 = MyRequests.post("/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+        user_id_from_auth_method = self.get_json_value(response1, "user_id")
+
+        response2 = MyRequests.get(
+            f"/user/{user_id_from_auth_method}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+        Assertions.assert_json_has_keys(response2, ["username", "email", "firstName", "lastName"])
+
+    def test_get_user_details_with_another_id(self):
+
+        # Get token and cookie for the first user
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response1 = MyRequests.post("/user/login", data=data)
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+
+        # Get user_id for the second user
+
+        data = self.prepare_registration_data()
+        response = MyRequests.post("/user", data=data)
+        another_user_id = BaseCase.get_json_value(self, response, "id")
+
+        # Send request to get user data with another user id
+        response2 = MyRequests.get(
+            f"/user/{another_user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+
+        Assertions.assert_json_has_key(response2, "username")
+        Assertions.assert_json_has_not_key(response2, "email")
+        Assertions.assert_json_has_not_key(response2, "firstName")
+        Assertions.assert_json_has_not_key(response2, "lastName")
+
+
+
+
