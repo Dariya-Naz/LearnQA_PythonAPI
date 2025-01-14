b1121073ac738d7446172de466e044af9b27464a
DN <ryabkova.darya@gmail.com>
2025-01-14 23:14:21 +0300

    test_user_register_with_tags

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -3,6 +3,7 @@ import pytest
 from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+import allure
 
 
 class TestUserRegister(BaseCase):
@@ -13,7 +14,7 @@ class TestUserRegister(BaseCase):
         ("lastName"),
         ("email")
     ]
-
+    @allure.severity(allure.severity_level.BLOCKER)
     def test_create_user_successfully(self):
         data = self.prepare_registration_data()
 
@@ -22,6 +23,7 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response, "id")
 
+    @allure.severity(allure.severity_level.NORMAL)
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
         data = self.prepare_registration_data(email)
@@ -31,6 +33,7 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content '{response.content}'"
 
+    @allure.severity(allure.severity_level.MINOR)
     def test_create_user_without_at(self):
         email = 'dariyaexample.com'
         data = self.prepare_registration_data(email)
@@ -39,6 +42,7 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 400)
         Assertions.assert_response_text(response, "Invalid email format")
 
+    @allure.severity(allure.severity_level.MINOR)
     @pytest.mark.parametrize('element_for_delete', element_for_delete)
     def test_create_user_with_one_missed_parameter(self, element_for_delete):
         data = self.prepare_registration_data()
@@ -48,6 +52,7 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 400)
         Assertions.assert_response_text(response, f"The following required params are missed: {element_for_delete}")
 
+    @allure.severity(allure.severity_level.MINOR)
     def test_create_user_with_too_short_username(self):
         data = self.prepare_registration_data(self, username='a')
 
@@ -55,6 +60,7 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 400)
         Assertions.assert_response_text(response, 'The value of \'username\' field is too short')
 
+    @allure.severity(allure.severity_level.MINOR)
     def test_create_user_with_too_long_username(self):
         long_user_name = 'hfudhfsjdfljsdlksdjcldsjsjedpijedjeojdpoejdpjepdojddfjvglkdfjlksdjflsjdfcoihsdicugbsdbcisdnlicksdlkcnlskdnclsndclknsdlkcfnslkdnclksdnclklksdnlckndpoejdpjepdojddfjvglkdfjlksdjflsjdfcoihsdicugbsdbcisdnlicksdlkcnlkdnclsndclknsdlkcnslkdnclksdnclksdsdnlckn'
         data = self.prepare_registration_data(self, username=long_user_name)
